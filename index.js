1c81a584a235c4955e62b163726419e54f2c8660
Roger Mendez <roger61087@gmail.com>
2021-10-01 17:57:31 -0400

    commit tree 8d85064c742eb0945351a719d7ceae02be5ce640

--- index.js
+++ index.js
@@ -1,23 +1,89 @@
-/* Your Code Here */
+let testEmployees = [
+  ["gary", "worm", "security", 3],
+  ["tom", "holland", "professor", 4],
+  ["harry", "styles", "musician", 4],
+  ["bob", "sagget", "actor", 4],
+];
 
-/*
- We're giving you this function. Take a look at it, you might see some usage
- that's new and different. That's because we're avoiding a well-known, but
- sneaky bug that we'll cover in the next few lessons!
-
- As a result, the lessons for this function will pass *and* it will be available
- for you to use if you need it!
- */
-
-const allWagesFor = function () {
-    const eligibleDates = this.timeInEvents.map(function (e) {
-        return e.date
-    })
-
-    const payable = eligibleDates.reduce(function (memo, d) {
-        return memo + wagesEarnedOnDate.call(this, d)
-    }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
-
-    return payable
+function createEmployeeRecord(employee) {
+  let record = {
+    firstName: employee[0],
+    familyName: employee[1],
+    title: employee[2],
+    payPerHour: employee[3],
+    timeInEvents: [],
+    timeOutEvents: [],
+  };
+  return record;
 }
 
+// console.log(createEmployeeRecord(testEmployees));
+
+function createEmployeeRecords(employeesArray) {
+  return employeesArray.map(createEmployeeRecord);
+}
+
+// console.log(createEmployeeRecords(testEmployees));
+
+function createTimeInEvent(dateTime) {
+  let [date, hour] = dateTime.split(" ");
+  hour = parseInt(hour);
+  let type = "TimeIn";
+  this.timeInEvents.push({ type, hour, date });
+  return this;
+}
+// console.log(
+//   createTimeInEvent(createEmployeeRecords(testEmployees)[1], "08/12/2021 800")
+// );
+
+function createTimeOutEvent(dateTime) {
+  let [date, hour] = dateTime.split(" ");
+  hour = parseInt(hour);
+  let type = "TimeOut";
+  this.timeOutEvents.push({ type, hour, date });
+  return this;
+}
+// console.log(
+//   createTimeOutEvent(createEmployeeRecords(testEmployees)[1], "08/12/2021 1800")
+// );
+
+function hoursWorkedOnDate(workDate) {
+  let clockedIn = this.timeInEvents
+    .filter((element) => element.date === workDate)
+    .map((element) => element.hour);
+
+  let clockedOut = this.timeOutEvents
+    .filter((element) => element.date === workDate)
+    .map((element) => element.hour);
+
+  return (clockedOut - clockedIn) / 100;
+}
+
+function wagesEarnedOnDate(date) {
+  return this.payPerHour * hoursWorkedOnDate.call(this, date);
+}
+
+function findEmployeeByFirstName(array, firstN) {
+  return array.find((object) => object.firstName === firstN);
+}
+
+function calculatePayroll(array) {
+  return array
+    .map((object) => allWagesFor.call(object))
+    .reduce((a, b) => (a = a + b), 0);
+}
+
+const allWagesFor = function () {
+  const eligibleDates = this.timeInEvents.map(function (e) {
+    return e.date;
+  });
+
+  const payable = eligibleDates.reduce(
+    function (memo, d) {
+      return memo + wagesEarnedOnDate.call(this, d);
+    }.bind(this),
+    0
+  ); // <== Hm, why did we need to add bind() there? We'll discuss soon!
+
+  return payable;
+};
